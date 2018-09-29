bdd39ee7122c91326847e28d0cecd52e87757141
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2018-09-29 16:44:47 +0200

    Create skeleton of the payment contract

---
+++ backend/contracts/payments.sol
@@ -0,0 +1,58 @@
+pragma solidity ^0.4.2;
+
+contract Payments {
+    address[] users;
+    Provider[] providers;
+    
+    struct Provider {
+        address providerAddress;
+        string name;
+        Service[] services;
+    }
+    
+    struct Service {
+        string name;
+        uint8 price;
+    }
+    
+    function becomeUser() public {
+        // check if the caller is already a registered user
+        
+        // add caller to the user list
+    }
+    
+    function becomeProvider() public {
+        // check if the caller is already a registered provider
+        
+        // add caller to the provider list
+    }
+    
+    function offerService(string serviceName, uint8 servicePrice) public {
+        // caller is expected to be provider
+        // check if caller is registered provider
+        // check if the given service is already in the list
+        
+        // add service to the list of services of the given provider
+    }
+    
+    function getProviders() public {
+        // return a list of registered providers
+    }
+    
+    function getUsers() public {
+        // return a list of registered users
+    }
+    
+    function getOffers(address provider) public {
+        // check if given provider is registered
+        
+        // return a list of services the given provider offers
+    }
+    
+    function makePayment(address provider, string service) public payable {
+        // check if given provider is registered
+        // check if given provider offers required service
+        
+        // make payment -> substract monye from caller, send money to the provider
+    }
+}
