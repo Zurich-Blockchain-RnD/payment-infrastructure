c47e8dd6c4e90f34f018c785419d799cbb1696f9
Tomasvrba <vrba.tomas@protonmail.com>
2018-10-03 19:30:46 +0200

    change name to backedn-java

---
+++ backend-java/application-servers/payment-backend/src/main/java/com/zurichdev/payback/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.zurichdev.payback.config.audit;
---
+++ backend-java/contracts/payments.sol
@@ -0,0 +1,59 @@
+pragma solidity ^0.4.2;
+
+contract Payments {
+    address[] users;
+    Provider[] providers;
+    
+    struct Provider {
+        address providerAddress;
+        string name;
+        Service[] services;
+    }
+    
+    struct Service {
+        string name;
+        uint8 price;
+    }
+    
+    function addUser() public {
+        // check if the caller is already a registered user
+        
+        // add caller to the user list
+    }
+    
+    function addProvider() public {
+        // check if the caller is already a registered provider
+        
+        // add caller to the provider list
+    }
+    
+    function offerService(string serviceName, uint8 servicePrice) public {
+        // caller is expected to be provider
+        // check if caller is registered provider
+        // check if the given service is already in the list
+        
+        // add service to the list of services of the given provider
+    }
+    
+    function getProviders() public {
+        // return a list of registered providers
+    }
+    
+    function getUsers() public {
+        // return a list of registered users
+    }
+    
+    function getOffers(address provider) public {
+        // check if given provider is registered
+        
+        // return a list of services the given provider offers
+    }
+    
+    function makePayment(address provider, string service) public payable {
+        // check if the caller is registered user
+        // check if given provider is registered
+        // check if given provider offers required service
+        
+        // make payment -> substract money from caller, send money to the provider
+    }
+}
