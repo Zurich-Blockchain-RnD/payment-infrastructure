6b059b4c9db08cd675382cbd0a5f86ccd4233fed
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2018-10-17 20:38:45 +0200

    Removing a duplicate folder

--- backend-java/application-servers/payment-backend/src/main/java/com/zurichdev/payback/config/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Spring Framework configuration files.
- */
-package com.zurichdev.payback.config;
--- backend-java/application-servers/payment-backend/src/main/java/com/zurichdev/payback/service/dto/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Data Transfer Objects.
- */
-package com.zurichdev.payback.service.dto;
